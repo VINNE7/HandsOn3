996775d8286c62337365520411ff8949bbe5ad5b
Marcus Vinicius <marcusv.ribas@gmail.com>
2022-05-23 20:11:46 -0300

    add handle error to app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const handleError = require("./middlewares/handleError");
 
 require('./database');
 
@@ -9,6 +10,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(routes);
+app.use(handleError);
 
 
 app.listen(port,() => {
