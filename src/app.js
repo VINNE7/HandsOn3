0228143db367663be269906c6bedf3aadaeaca94
Marcus Vinicius <marcusv.ribas@gmail.com>
2022-05-23 13:40:13 -0300

    fix app.use(routes) location

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 
+require('./database');
+
 const app = express();
 
 const port = 3000;
 
-app.use(routes);
 app.use(express.json());
+app.use(routes);
 
 
 app.listen(port,() => {
